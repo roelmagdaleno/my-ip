0d66f1798e67fcc207c35093cbce268e6aee1662
Roel Magdaleno <roelmagdaleno@gmail.com>
2025-04-11 12:09:40 -0600

    We don't need functions anymore

--- functions/ip.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest({ request }) {
-    // Cloudflare adds the CF-Connecting-IP header to the request.
-    const ip = request.headers.get("CF-Connecting-IP") || "Unknown IP";
-
-    // Return the IP as JSON.
-    return new Response(JSON.stringify({ ip }), {
-        headers: { "Content-Type": "application/json" },
-    });
-}
